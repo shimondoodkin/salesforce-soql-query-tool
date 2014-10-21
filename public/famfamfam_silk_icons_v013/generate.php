4dd009218de7aa23868455037a3ae79ddb6e1aa1
Shimon Doodkin <helpmepro1@gmail.com>
2014-10-21 07:32:05 +0000

    $@

--- public/famfamfam_silk_icons_v013/generate.php
+++
@@ -1,9 +0,0 @@
-<?php
- $files = scandir("./icons"); //get the list of files
-  $files = array_diff($files,array(".","..")); //remove directories
-  foreach ($files as $key => $file) {
-  $name = preg_replace('/\.[a-z]{3}$/i','',$file); //remove extension
-  $name = preg_replace('/[^\w\d]|_/','-',$name); //replace weird characters with dash
-  echo ".ui-icon-silk-$name  { background-position: 0 0; background-image:url(images/$file) !important;}\n"; //output the rule
- }
-?>
